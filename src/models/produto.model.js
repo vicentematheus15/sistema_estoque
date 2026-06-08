93a3e0d74816ae54d47d0138bd6b7f0da8ecc648
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-06-08 20:28:12 -0300

    feat: define Produto model with attributes and validations

---
+++ src/models/produto.model.js
@@ -0,0 +1,33 @@
+import sequelize from "../database/database.js";
+import { DataTypes } from "sequelize";
+
+export const Produto = sequelize.define('Produto',
+    {
+        id_produto: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        nome: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        categoria: {
+            type: DataTypes.ENUM('Produtos Quimicos', 'Ferramentas e Acessorios', 'Equipamentos de Protecao'),
+            allowNull: false,
+            set(value) {
+                this.setDataValue(
+                    'categoria',
+                    value?.toLowerCase()
+                )
+            }
+        },
+        valor: {
+            type: DataTypes.DECIMAL,
+            allowNull: false
+        }
+    }, {
+        tableName: 'produtos',
+        timestamps: true
+    }
+)
