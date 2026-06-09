718b5da47cc39491fb93570fd754d88537cd796a
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-06-09 20:57:23 -0300

    feat: implement entradaProdutos function with input validation for product entry

--- src/controllers/movimentacao.controller.js
+++ src/controllers/movimentacao.controller.js
@@ -5,6 +5,12 @@ import {Produto, Movimentacao}  from '../models/movimentacao.model.js';
 
 export async function entradaProdutos(req, res) {
    try {
+    const {id_produto, tipo, quantidade } = req.body;
+
+    if (!id_produto || !tipo || !quantidade) {
+        return res.status(400).json({erro: 'Dados incompletos!'})
+    }
+
     
    } catch (error) {
     
