3f4f700a1e1923404e865ef2551ef1163fa8ecab
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-06-09 20:50:22 -0300

    feat: add entradaProdutos function in movimentacao.controller.js for product entry handling

---
+++ src/controllers/movimentacao.controller.js
@@ -0,0 +1,10 @@
+import sequelize from '../database/database.js';
+import { Op } from 'sequelize'
+
+export async function entradaProdutos(req, res) {
+   try {
+    
+   } catch (error) {
+    
+   } 
+}
