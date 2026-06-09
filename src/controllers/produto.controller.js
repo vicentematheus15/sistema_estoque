79c73d5086f81d819d3c02410201b6ca49531a68
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-06-08 21:39:27 -0300

    feat: implement cadastrarProdutos function in produto.controller.js for product registration

--- src/controllers/produto.controller.js
+++ src/controllers/produto.controller.js
@@ -1,6 +1,32 @@
 import {Produto, Movimentacao}  from '../models/movimentacao.model.js';
-import sequelize from '../database/database.js'
+import sequelize from '../database/database.js';
+import { Op } from 'sequelize'
 
-export async function listarProdutos(req, res) {
-    
+
+
+export async function cadastrarProdutos (req, res) {
+    try {
+        const {nome, categoria, quantidade, valor_unitario } = req.body;
+        if (!nome || !categoria || !quantidade || !valor_unitario){
+            return res.status(400).json({erro: 'Dados incompletos!'})
+        }
+
+        const novoProduto = await Produto.create({
+            nome: nome,
+            categoria: categoria,
+            quantidade: quantidade,
+            valor_unitario: valor_unitario
+        });
+
+        return res.status(201).json({
+            nome: novoProduto.nome,
+            categoria: novoProduto.categoria,
+            quantidade: novoProduto.quantidade,
+            valor_unitario: novoProduto.valor_unitario,
+            createdAt: novoProduto.createdAt
+        })
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ erro: 'Erro interno do servidor' });
+    }
 }
