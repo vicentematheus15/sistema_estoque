c36b21c7a6206ae2835cef01e652ff214b94edf2
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-06-08 21:40:39 -0300

    feat: add route for cadastrarProdutos in produto.routes.js

--- src/routes/produto.routes.js
+++ src/routes/produto.routes.js
@@ -4,6 +4,7 @@ import * as controller from '../controllers/produto.controller.js';
 const produtoRoutes = Router();
 
 produtoRoutes.get('listar', controller.listarProdutos)
+produtoRoutes.get('cadastrar', controller.cadastrarProdutos)
 
 
 export default produtoRoutes;
