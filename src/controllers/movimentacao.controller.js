1856c9a463435f7555ed9861ddf8cc7f86b7338e
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-06-09 20:51:29 -0300

    refactor: clean up imports in movimentacao.controller.js and movimentacoes.routes.js

--- src/controllers/movimentacao.controller.js
+++ src/controllers/movimentacao.controller.js
@@ -1,5 +1,7 @@
 import sequelize from '../database/database.js';
-import { Op } from 'sequelize'
+import { Op } from 'sequelize';
+import {Produto, Movimentacao}  from '../models/movimentacao.model.js';
+
 
 export async function entradaProdutos(req, res) {
    try {
--- src/routes/movimentacoes.routes.js
+++ src/routes/movimentacoes.routes.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import * as controller from '../controllers/movimentacao.controller.js';
-import {Produto, Movimentacao}  from '../models/movimentacao.model.js';
 
 const movimentacoesRoutes = Router();
 
