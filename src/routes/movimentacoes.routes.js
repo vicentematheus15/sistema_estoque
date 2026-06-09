3e0e10766f0f5c3ae1b0e239c75fde45e4688751
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-06-09 20:50:42 -0300

    feat: add PUT route for entradaProdutos in movimentacoes.routes.js

--- src/routes/movimentacoes.routes.js
+++ src/routes/movimentacoes.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import * as controller from '../controllers/movimentacao.controller.js';
+import {Produto, Movimentacao}  from '../models/movimentacao.model.js';
 
 const movimentacoesRoutes = Router();
 
-// produtoRoutes.get('listar', controller.listarProdutos)
+movimentacoesRoutes.put('entrada', controller.entradaProdutos)
 
 
 export default movimentacoesRoutes;
