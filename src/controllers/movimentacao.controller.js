dae1ac71173534384c7b5a943f92747dd69a6ac8
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-06-09 21:03:31 -0300

    feat: enhance entradaProdutos function with additional validation for product type and existence

--- src/controllers/movimentacao.controller.js
+++ src/controllers/movimentacao.controller.js
@@ -7,11 +7,21 @@ export async function entradaProdutos(req, res) {
    try {
     const {id_produto, tipo, quantidade } = req.body;
 
+    
     if (!id_produto || !tipo || !quantidade) {
+        if(!tipo.toLowerCase() === 'entrada'){
+            return res.status(400).json({erro: 'Tipo de movimentação incorreta!'})
+        }
         return res.status(400).json({erro: 'Dados incompletos!'})
     }
 
-    
+    const produtoAlvo = await Produto.findByPk(id_produto)
+    if (!produtoAlvo) {
+        return res.status(400).json({erro: 'Produto não encontrado!'})
+    }
+
+
+
    } catch (error) {
     
    } 
