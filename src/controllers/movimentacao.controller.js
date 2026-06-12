a4d13945b5fcae342847add3a7631688ab47a2a4
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-06-11 21:32:39 -0300

    feat: implement entradaProdutos and saidaProdutos functions with validation and alerts

--- src/controllers/movimentacao.controller.js
+++ src/controllers/movimentacao.controller.js
@@ -1,49 +1,98 @@
 import sequelize from '../database/database.js';
 import { Op } from 'sequelize';
-import {Produto, Movimentacao}  from '../models/movimentacao.model.js';
+import { Produto, Movimentacao } from '../models/movimentacao.model.js';
 
 
 export async function entradaProdutos(req, res) {
-   try {
-    const {id_produto, quantidade } = req.body;
+    try {
+        const { id_produto, quantidade } = req.body;
 
-    if (!id_produto || !quantidade) {
-        return res.status(400).json({erro: 'Dados incompletos!'})
+        if (!id_produto || !quantidade) {
+            return res.status(400).json({ erro: 'Dados incompletos!' })
+        }
+
+        const produtoAlvo = await Produto.findByPk(id_produto)
+        if (!produtoAlvo) {
+            return res.status(400).json({ erro: 'Produto não encontrado!' })
+        }
+
+        //registra a movimentacao
+        const novaMovimentacao = await Movimentacao.create({
+            id_produto,
+            tipo: 'entrada',
+            quantidade,
+            data_movimentacao: new Date()
+        })
+
+        //incrementa a qtd de produtos da movimentação na qtd de produtos em estoque
+        await produtoAlvo.increment('quantidade', { by: Number(quantidade) });
+
+        produtoAlvo.reload() //atualiza a quantidade após a movimentacao
+
+        const resposta = {
+            mensagem: 'Entrada registrada com sucesso!',
+            movimentacao: novaMovimentacao
+        }
+
+        //anexa um alerta na resposta caso a quantidade de produto atinja um limite
+        if (produtoAlvo.quantidade >= 100) {
+            resposta.alerta = `Produto atingiu o limite máximo de estoque! Quantidade atual: ${produtoAlvo.quantidade}`;
+        }
+
+
+        return res.status(201).json(resposta);
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ erro: 'Erro interno do servidor' });
     }
+}
 
-    const produtoAlvo = await Produto.findByPk(id_produto)
-    if (!produtoAlvo) {
-        return res.status(400).json({erro: 'Produto não encontrado!'})
+export async function saidaProdutos(req, res) {
+    try {
+        const {id_produto, quantidade} = req.body;
+        
+        if(!id_produto || !quantidade){
+            return res.status(400).json({erro: 'Dados incompletos'})
+        }
+
+        const produtoAlvo = await Produto.findByPk(id_produto)
+        if(!produtoAlvo){
+            return res.status(404).json({erro: 'Produto não encontrado!'})
+        }
+
+        //valida se ao retirar do esoqu a quantidade indicada, o estoque ficara menor que zero
+        if(produtoAlvo.quantidade - Number(quantidade) < 0){
+            return res.status(400).json({ erro: 'Quantidade insuficiente em estoque!'})
+        }
+        
+        //cria movimentacao de saida
+        const novaMovimentacao = await Movimentacao.create({
+            id_produto,
+            tipo: 'saida',
+            quantidade,
+            data_movimentacao: new Date()
+        })
+        
+        //decrementa a qtd de produtos da movimentação na qtd de produtos em estoque
+        await produtoAlvo.decrement('quantidade', { by: quantidade })
+        await produtoAlvo.reload() //recarrega a nova quantidade de produtos apos movimentacao
+        
+
+        const resposta = {
+            mensagem: 'Saída registrada com sucesso',
+            movimentacao: novaMovimentacao
+        }
+
+        //anexa um alerta na resposta caso a quantidade de produto atinja um limite
+        if(produtoAlvo.quantidade === 0){
+            resposta.alerta = 'Produto atingiu o limite mínimo de estoque!'
+        }
+        
+        return res.status(201).json(resposta)
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ erro: 'Erro interno do servidor' });
     }
-
-    //registra a movimentacao
-    const novaMovimentacao = await Movimentacao.create({
-        id_produto,
-        tipo: 'entrada',
-        quantidade,
-        data_movimentacao: new Date()
-    })
-
-    //incrementa a qtd de produtos da movimentação na qtd de produtos em estoque
-    await produtoAlvo.increment('quantidade', { by: Number(quantidade) });
-
-    produtoAlvo.reload() //atualiza a quantidade após a movimentacao
-
-    const resposta = {
-        mensagem: 'Entrada registrada com sucesso!',
-        movimentacao: novaMovimentacao
-    }
-
-    //anexa um alerta na resposta caso a quantidade de produto atinja um limite
-    if(produtoAlvo.quantidade >= 100){
-        resposta.alerta = `Produto atingiu o limite máximo de estoque! Quantidade atual: ${produtoAlvo.quantidade}`;
-    }
-
-
-    return res.status(201).json(resposta);
-
-   } catch (error) {
-    console.error(error);
-    return res.status(500).json({ erro: 'Erro interno do servidor' });
-   } 
 }
--- src/routes/movimentacoes.routes.js
+++ src/routes/movimentacoes.routes.js
@@ -4,6 +4,8 @@ import * as controller from '../controllers/movimentacao.controller.js';
 const movimentacoesRoutes = Router();
 
 movimentacoesRoutes.post('/entrada', controller.entradaProdutos)
+movimentacoesRoutes.post('/saida', controller.saidaProdutos)
+
 
 
 export default movimentacoesRoutes;
